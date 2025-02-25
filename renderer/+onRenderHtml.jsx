bb84e02ae729566de3c09382549e136b8da4b48c
Corey Donahue <cdartswebdev@gmail.com>
2025-02-25 10:06:44 -0700

    updated tab logo with new .png

--- renderer/+onRenderHtml.jsx
+++ renderer/+onRenderHtml.jsx
@@ -3,7 +3,7 @@ export { onRenderHtml }
 import ReactDOMServer from 'react-dom/server'
 import { Layout } from './Layout'
 import { escapeInject, dangerouslySkipEscape } from 'vike/server'
-import logoUrl from './logo.svg'
+import logoUrl from './logo.png'
 import { getPageTitle } from './getPageTitle'
 
 // Load environment variables
